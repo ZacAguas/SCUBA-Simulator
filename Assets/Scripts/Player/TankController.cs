fae5c0c7b9379942249a20e97f6f53d11afcfd45
ZacAguas <zacaguas@gmail.com>
2023-06-04 11:04:05 +0100

    Added Events for TankController low air and out of air

--- Assets/Scripts/Player/TankController.cs
+++ Assets/Scripts/Player/TankController.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TankController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float CurrentTankPressure {
+        get => currentTankPressure;
+        private set => Mathf.Max(0, currentTankPressure);
+    }
+    private float currentTankPressure; // backing field, should have no usages, use property instead
+    
+    [SerializeField] private float maxTankPressure; // max pressure of tank in bar
+    [SerializeField] private float lowTankPercentageThreshold;  // point at which air in tank is considered 'low' eg. 0.33 = rule of thirds
+    [SerializeField] private GameEvent onLowTankPressure; // triggered when tank reaches 'low' threshold
+    [SerializeField] private GameEvent onOutOfAir; // triggered when tank reaches 0
+
+    private void Start()
     {
-        
+        CurrentTankPressure = maxTankPressure;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CheckTankPressure()
     {
-        
+        if (CurrentTankPressure <= maxTankPressure * lowTankPercentageThreshold) // we have 'low' pressure
+            onLowTankPressure.Invoke(); // raise low pressure event
+
+        if (CurrentTankPressure == 0)
+            onOutOfAir.Invoke();
+
     }
+    private void FixedUpdate()
+    {
+        CheckTankPressure();
+    }
+    
 }
