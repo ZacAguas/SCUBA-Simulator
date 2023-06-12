8822a1952244a33eaec515b71cdc4f338520dd1f
ZacAguas <zacaguas@gmail.com>
2023-06-12 12:25:32 +0100

    Pressure gauge labels created

--- Assets/Scripts/UI/PressureGauge.cs
+++ Assets/Scripts/UI/PressureGauge.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Searcher;
 using UnityEngine;
 
 public class PressureGauge : MonoBehaviour
 {
     [SerializeField] private GameObject needle;
     [SerializeField] private GameObject labelTemplate;
+    [SerializeField] private int numLabels;
 
     private Vector3 startingRotation; // the rotation of the needle at the 0 o'clock position
     private float maxPressureAngle; // angle at 100% pressure, depends on startingRotation
@@ -51,19 +53,22 @@ public class PressureGauge : MonoBehaviour
 
     private void CreateLabels()
     {
-        int numLabels = 10;
         for (int i = 0; i <= numLabels; i++)
         {
             GameObject label = Instantiate(labelTemplate, transform);
-            float normalisedLabelPressure = (float)i / numLabels;
-            var labelAngle = zeroPressureAngle + normalisedLabelPressure * totalAngleRange;
+            float normalisedLabelPressure = (float)i / numLabels; // 0 to 1 value
+            var labelAngle = zeroPressureAngle + normalisedLabelPressure * totalAngleRange; // how much the current label should be rotated from zero angle
 
             var angles = label.transform.localEulerAngles;
-            label.transform.localEulerAngles = new Vector3(labelAngle, angles.y, angles.z);
+            label.transform.localEulerAngles = new Vector3(labelAngle - 90, angles.y - 90, angles.z + 90);
 
             // gross hardcoded value NOTE: LABEL MUST BE CALLED "Label" in template
-            label.transform.Find("Label").GetComponent<TextMeshPro>().text =
-                Mathf.RoundToInt(normalisedLabelPressure * maxPressure).ToString();
+            TextMeshPro labelText = label.transform.Find("Label").GetComponent<TextMeshPro>();
+            labelText.text = Mathf.RoundToInt(normalisedLabelPressure * maxPressure).ToString();
+            var textAngles = labelText.transform.localEulerAngles;
+            labelText.transform.localEulerAngles = new Vector3(textAngles.x , textAngles.y + labelAngle, textAngles.z); // un-rotate labels
+            if (i < numLabels / 2) // on the left side of the dial
+                labelText.alignment = TextAlignmentOptions.Left;
             label.SetActive(true);
         }
         
